9f46807eb76ede533ae1a3df3c982fbffb59f9c4
manohar0789 <manuhere2821@gmail.com>
2024-04-18 11:13:23 +0530

    first

---
+++ Validation.js
@@ -0,0 +1,9 @@
+// var form = document.forms["myForm"];
+
+// var nameInp = document.getElementById('name-inp').value;
+// var emailInp = document.getElementById('email-inp').value;
+// var password = document.getElementById('password-inp').value;
+// var nameErr = document.getElementById('name-error');
+// var emailErr = document.getElementById('email-error');
+// var passwordErr = document.getElementById('password-error');
+
